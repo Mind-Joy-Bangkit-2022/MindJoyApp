db9a8978b12695dc166b56442bc3d4854aee1ef7
Muhammad Taufik Hidayat <79823539+Taufik322@users.noreply.github.com>
2022-05-26 19:07:26 +0700

    add bottom navbar

--- app/src/main/java/com/example/mindjoy/home/MainActivity.kt
+++ app/src/main/java/com/example/mindjoy/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mindjoy
+package com.example.mindjoy.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mindjoy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
