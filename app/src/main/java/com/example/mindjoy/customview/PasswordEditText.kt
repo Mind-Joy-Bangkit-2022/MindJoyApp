c27d7dd679150fd70fcc5fae3d276c5c9f3a5e5e
Aldino Reisnanda <94807337+Sumiyarang@users.noreply.github.com>
2022-05-29 02:56:48 +0700

    # Added register activity # Fixed some layout for login activity

---
+++ app/src/main/java/com/example/mindjoy/customview/PasswordEditText.kt
@@ -0,0 +1,49 @@
+package com.example.mindjoy.customview
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class PasswordEditText : AppCompatEditText, View.OnTouchListener {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                error = if(s.length in 1..5){
+                    "Password must be 6 character or more!"
+                } else{
+                    null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+    }
+
+   override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+        return false
+    }
+}
+
+
--- app/src/main/java/com/example/mindjoy/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/mindjoy/ui/register/RegisterActivity.kt
@@ -1,12 +1,56 @@
 package com.example.mindjoy.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
 import com.example.mindjoy.R
+import com.example.mindjoy.customview.ClearImgEditText
+import com.example.mindjoy.customview.PasswordEditText
+import com.example.mindjoy.databinding.ActivityRegisterBinding
+import com.example.mindjoy.ui.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var etName: ClearImgEditText
+    private lateinit var etUsername: ClearImgEditText
+    private lateinit var edPw: PasswordEditText
+    private lateinit var btnSignup: Button
+    private lateinit var tvHaveAccount: TextView
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        etName = findViewById(R.id.et_name)
+        etUsername = findViewById(R.id.et_username)
+        edPw = findViewById(R.id.et_password)
+        btnSignup = findViewById(R.id.btn_signup)
+        tvHaveAccount = findViewById(R.id.tv_have_account)
+
+        onClickListener()
+    }
+
+    private fun onClickListener(){
+        btnSignup.setOnClickListener{
+            moveToLogin()
+        }
+
+        tvHaveAccount.setOnClickListener {
+            moveToLogin()
+        }
+    }
+
+    private fun moveToLogin(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        },500)
+
     }
 }
