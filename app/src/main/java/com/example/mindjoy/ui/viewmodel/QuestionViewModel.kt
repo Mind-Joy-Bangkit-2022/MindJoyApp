165318c10b0ba70add174f754841e21f2c3fb4a6
Muhammad Taufik Hidayat <79823539+Taufik322@users.noreply.github.com>
2022-06-13 11:18:21 +0700

    clean code

--- app/src/main/java/com/example/mindjoy/ui/viewmodel/QuestionViewModel.kt
+++ app/src/main/java/com/example/mindjoy/ui/viewmodel/QuestionViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import com.example.mindjoy.network.*
 import retrofit2.Call
 import retrofit2.Response
+import java.net.SocketTimeoutException
 
 class QuestionViewModel : ViewModel() {
 
